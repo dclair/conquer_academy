fc94106c70ed82ad435e1cb022dc82d7127acc0a
JM Declara Lanzas <jmdclair@gmail.com>
2025-04-27 15:33:50 +0100

    reconfigurando vite.config.js para crear multiples páginas del proyecto

--- conquer_academy/vite.config.js
+++ conquer_academy/vite.config.js
@@ -1,5 +1,25 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/conquer_academy/',  // Reemplaza 'TU_REPO' por el nombre de tu repositorio en GitHub
+  base: '/conquer_academy/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        contacto: './contacto.html',  // Incluye cada archivo HTML aquí si es necesario
+        cursos: './cursos.html',
+        curso1: './curso1.html',
+        curso2: './curso2.html',
+        curso3: './curso3.html',
+        blog: './blog.html',
+        login: './login.html',
+        noticia1: './noticia1.html',
+        noticia2: './noticia2.html',
+        quienes_somos: './quienes_somos.html',
+        registro: './registro.html',
+
+      }
+    }
+  }
 });
+
